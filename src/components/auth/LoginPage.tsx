fbeb6a0f1949c2e32cf9ad85bea3e79548a003d3
Siddharth Lamba <122638975+lambasid@users.noreply.github.com>
2025-01-24 11:56:57 -0500

    Update LoginPage.tsx
    Updated the path for the icon

--- src/components/auth/LoginPage.tsx
+++ src/components/auth/LoginPage.tsx
@@ -22,7 +22,7 @@ export const LoginPage = () => {
         <div className="flex flex-col items-center justify-center">
           <div className="mx-auto w-24 h-24 hover:scale-110 transition-transform duration-300 ease-in-out hover:rotate-6">
             <img
-              src="/public/images/lock.png"
+              src="/public/lock.png"
               alt="Lock Icon"
               className="w-full h-full object-contain animate-bounce"
             />
